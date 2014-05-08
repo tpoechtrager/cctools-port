9ac9eeccec2ef239f7d8c91d06db08a7cd4b4b80
Thomas Pöchtrager <t.poechtrager@gmail.com>
2014-05-08 21:30:38 +0200

    ld64: enable all architectures

--- cctools/ld64/src/configure.h
+++ cctools/ld64/src/configure.h
@@ -18,18 +18,24 @@ extern "C" {
 
 #define CPU_SUBTYPE_X86_ALL     ((cpu_subtype_t)3)
 
+#define SUPPORT_ARCH_armv4t 1
+#define SUPPORT_ARCH_armv5 1
+#define SUPPORT_ARCH_armv6 1
+#define SUPPORT_ARCH_armv7 1
+#define SUPPORT_ARCH_armv7f 1
+#define SUPPORT_ARCH_armv7k 1
+#define SUPPORT_ARCH_armv7s 1
+#define SUPPORT_ARCH_armv6m 1
+#define SUPPORT_ARCH_armv7m 1
+#define SUPPORT_ARCH_armv7em 1
+#define SUPPORT_ARCH_armv8 1
+#define SUPPORT_ARCH_arm64 1
+#define SUPPORT_ARCH_arm64v8 1
+#define SUPPORT_ARCH_i386 1
+#define SUPPORT_ARCH_x86_64 1
+#define SUPPORT_ARCH_x86_64h 1
 
-#define SUPPORT_ARCH_i386  1
-#define SUPPORT_ARCH_x86_64  1
-#define SUPPORT_ARCH_armv4t  1
-#define SUPPORT_ARCH_armv5  1
-#define SUPPORT_ARCH_armv6  1
-#define SUPPORT_ARCH_armv7  1
-#define SUPPORT_ARCH_armv7f  1
-#define SUPPORT_ARCH_armv7k  1
-#define SUPPORT_ARCH_armv7s  1
-#define SUPPORT_ARCH_arm64  1
-#define ALL_SUPPORTED_ARCHS  "i386 x86_64 armv4t armv5 armv6 armv7 armv7f armv7k armv7s arm64"
+#define ALL_SUPPORTED_ARCHS  "armv4t armv5 armv6 armv7 armv7f armv7k armv7s armv6m armv7m armv7em armv8 arm64 arm64v8 i386 x86_64 x86_64h"
 
 #define HW_NCPU      3
 #define CTL_HW      6
