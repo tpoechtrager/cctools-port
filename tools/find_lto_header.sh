57d4b7510d7314e32dde42f465447d1afd4a1b01
Thomas Pöchtrager <t.poechtrager@gmail.com>
2014-08-31 10:31:32 +0200

    link with -rpath to ease finding libLTO

--- tools/find_lto_header.sh
+++ tools/find_lto_header.sh
@@ -11,9 +11,10 @@ function try()
         set -e
         LLVM_INC_DIR=`$LLVM_CONFIG --includedir`
         LLVM_LIB_DIR=`$LLVM_CONFIG --libdir`
+        # Adding the include directory with -I causes the build to fail.
         ln -sf "$LLVM_INC_DIR/llvm-c/lto.h" "include/llvm-c/lto.h"
         mkdir -p tmp
-        echo -n "-L$LLVM_LIB_DIR -lLTO " > tmp/ldflags
+        echo -n "-Wl,-rpath,$LLVM_LIB_DIR,--enable-new-dtags -L$LLVM_LIB_DIR -lLTO " > tmp/ldflags
         echo -n "-DLTO_SUPPORT=1 " > tmp/cflags
         echo -n "-DLTO_SUPPORT=1 " > tmp/cxxflags
         echo -n "$LLVM_LIB_DIR" > tmp/ldpath
@@ -26,11 +27,13 @@ try "-3.2"
 try "-3.3"
 try "-3.4"
 try "-3.5"
+try "-3.6"
 try "-devel"
 
 try "32"
 try "33"
 try "34"
 try "35"
+try "36"
 
 exit 1
