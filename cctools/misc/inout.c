5e5b511a3eebd3fa9ece49bead85a1be29a25dfb
Thomas Pöchtrager <t.poechtrager@gmail.com>
2015-03-19 21:28:44 +0100

    fix more potential -D_FORTIFY_SOURCE crashers

--- cctools/misc/inout.c
+++ cctools/misc/inout.c
@@ -139,12 +139,32 @@ uint32_t narchs)
 			archs[i].toc_long_name = TRUE;
 		    }
 		    if(archs[i].members[j].object != NULL){
+			char tmp[32]; /* cctools-port */
+			int len;      /* cctools-port */
 			size += archs[i].members[j].object->object_size
 			   - archs[i].members[j].object->input_sym_info_size
 			   + archs[i].members[j].object->output_sym_info_size;
+#if 0
+			/*
+			 * cctools-port: This causes a crash with
+			 * -D_FORTIFY_SOURCE enabled ('\0' "overflows" the
+			 * buffer).
+			 */
 			sprintf(archs[i].members[j].ar_hdr->ar_size, "%-*ld",
 			       (int)sizeof(archs[i].members[j].ar_hdr->ar_size),
 			       (long)(size));
+#endif
+			/* cctools-port start */
+			len = sprintf(tmp, "%-*ld",
+			       (int)sizeof(archs[i].members[j].ar_hdr->ar_size),
+			       (long)(size));
+			if(len !=
+			  (int)sizeof(archs[i].members[j].ar_hdr->ar_size)){
+			    fprintf(stderr, "corrupted object file\n");
+			    abort();
+			}
+			memcpy(&archs[i].members[j].ar_hdr->ar_size, tmp, len);
+			/* cctools-port end */
 			/*
 			 * This has to be done by hand because sprintf puts a
 			 * null at the end of the buffer.
