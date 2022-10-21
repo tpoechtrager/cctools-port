3c08e44023332f987dbb99f42345520ef70d6096
philippe44 <philippe44@users.noreply.github.com>
2022-10-21 11:05:39 -0700

    was wrong PR

--- cctools/libstuff/ofile.c
+++ cctools/libstuff/ofile.c
@@ -1496,8 +1496,8 @@ struct ofile *ofile)
 		my_mach_error(r, "Can't vm_deallocate mapped memory for file: "
 			      "%s", ofile->file_name);
 	    }
+		munmap(ofile->file_addr, ofile->file_size);		
 	}
-	munmap(ofile->file_addr, ofile->file_size);
 	if(ofile->file_name != NULL)
 	    free(ofile->file_name);
 	if(ofile->arch_flag.name != NULL)
