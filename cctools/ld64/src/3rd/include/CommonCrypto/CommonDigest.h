8de30fa48909955952c3a0b34b9ffb47958d2194
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-04 23:04:27 -0500

    maybe fix iOS

--- cctools/ld64/src/3rd/include/CommonCrypto/CommonDigest.h
+++ cctools/ld64/src/3rd/include/CommonCrypto/CommonDigest.h
@@ -1,4 +1,5 @@
-#if defined(__APPLE__) && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1050
+#if defined(__APPLE__) && \
+    ((defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1050) || !defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__))
 
 #include_next <CommonCrypto/CommonDigest.h>
 
