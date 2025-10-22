b583d1e6efabd02aaa805683ee71f60e4b436811
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-22 17:31:12 -0400

    support iOS 1.x better

--- cctools/ld64/src/ld/ld.hpp
+++ cctools/ld64/src/ld/ld.hpp
@@ -199,6 +199,7 @@ static const PlatformVersion mac11_0		(Platform::macOS, 0x000B0000);
 static const PlatformVersion mac12_0		(Platform::macOS, 0x000C0000);
 static const PlatformVersion mac10_Future 	(Platform::macOS, 0x10000000);
 
+static const PlatformVersion iOS_1_0 		(Platform::iOS, 0x00010000);
 static const PlatformVersion iOS_2_0 		(Platform::iOS, 0x00020000);
 static const PlatformVersion iOS_3_1 		(Platform::iOS, 0x00030100);
 static const PlatformVersion iOS_4_2 		(Platform::iOS, 0x00040200);
@@ -259,7 +260,8 @@ static const PlatformSet simulatorPlatforms ( {
 
 //FIXME do we need to add simulatots to these?
 //FIXME Are the dates correct?
-static const VersionSet version2007		({mac10_4, iOS_2_0});
+static const VersionSet version2007		({mac10_4, iOS_1_0});
+static const VersionSet version2007Fall ({mac10_5, iOS_1_0});
 static const VersionSet version2008 	({mac10_5, iOS_2_0});
 static const VersionSet version2008Fall ({mac10_5, iOS_3_1});
 static const VersionSet version2009 	({mac10_6, iOS_3_1});
