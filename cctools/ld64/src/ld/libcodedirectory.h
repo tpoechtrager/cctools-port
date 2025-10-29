16a31c36993e838439b01ecfcfadcd63fc452165
Thomas Pöchtrager <t.poechtrager@gmail.com>
2025-10-29 08:29:43 +0100

    Comments

--- cctools/ld64/src/ld/libcodedirectory.h
+++ cctools/ld64/src/ld/libcodedirectory.h
@@ -5,7 +5,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#ifndef ATTRIBUTE_UNUSED __unused // ld64-port
+#ifndef ATTRIBUTE_UNUSED // ld64-port
 #define ATTRIBUTE_UNUSED __attribute__ ((unused))
 #endif
 
@@ -26,7 +26,7 @@ typedef size_t libcd_read_page (libcd *s, int page_no, size_t pos, size_t page_s
 typedef bool libcd_signature_generator (libcd *s, void *user_ctx, size_t signature_size, uint8_t *signature_buf);
 typedef void libcd_log_writer (char *stmt);
 
-void libcd_log_none (char *stmt ATTRIBUTE_UNUSED);
+void libcd_log_none (char *stmt ATTRIBUTE_UNUSED); // __unused -> ATTRIBUTE_UNUSED
 void libcd_log_stderr (char *stmt);
 #define libcd_log_default libcd_log_stderr
 
