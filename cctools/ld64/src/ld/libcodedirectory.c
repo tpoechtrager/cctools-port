7c738af28b4cbf11ecfb7adda2e574601711ece6
Thomas Pöchtrager <t.poechtrager@gmail.com>
2025-10-29 08:29:43 +0100

    Comments

--- cctools/ld64/src/ld/libcodedirectory.c
+++ cctools/ld64/src/ld/libcodedirectory.c
@@ -227,7 +227,7 @@ libcd_log_stderr (char *stmt)
 }
 
 void
-libcd_log_none (char *stmt ATTRIBUTE_UNUSED)
+libcd_log_none (char *stmt ATTRIBUTE_UNUSED) // ld64-port: __unused -> ATTRIBUTE_UNUSED
 {}
 
 static libcd_log_writer *_configured_log_writer = libcd_log_default;
